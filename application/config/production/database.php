82d492d09a4dc67098897ebac3ff48f342b064f5
Ryder Ross <ryross@gmail.com>
2011-04-06 08:25:36 -0700

    modifying the production config to show Konfig's power

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -4,54 +4,13 @@ return array
 (
 	'default' => array
 	(
-		'type'       => 'mysql',
 		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname     server hostname, or socket
-			 * string   database     database name
-			 * string   username     database username
-			 * string   password     database password
-			 * boolean  persistent   use persistent connections?
-			 *
-			 * Ports and sockets may be appended to the hostname.
-			 */
-			'hostname'   => 'localhost',
 			'database'   => 'PRODUCTION',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
 		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
 	),
 	'alternate' => array(
-		'type'       => 'pdo',
 		'connection' => array(
-			/**
-			 * The following options are available for PDO:
-			 *
-			 * string   dsn         Data Source Name
-			 * string   username    database username
-			 * string   password    database password
-			 * boolean  persistent  use persistent connections?
-			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
 			'username'   => 'PRODUCTION',
-			'password'   => 'r00tdb',
-			'persistent' => FALSE,
 		),
-		/**
-		 * The following extra options are available for PDO:
-		 *
-		 * string   identifier  set the escaping identifier
-		 */
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
 	),
 );
