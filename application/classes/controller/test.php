f34f9f37131bd9316fca4d9d6e9e0a4850b3a889
Ryder Ross <ryross@gmail.com>
2011-04-08 12:25:04 -0700

    adding 'env_' prefix to config directories

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -8,8 +8,8 @@ class Controller_Test extends Controller {
 		$this->view = View::factory('test');
 		$this->view->env = Kohana::$environment == Kohana::PRODUCTION ? 'production' : 'development';
 		$this->view->config = $config;
-		$this->view->production = Kohana::load(APPPATH.'config/production/database.php');
-		$this->view->development = Kohana::load(APPPATH.'config/development/database.php');
+		$this->view->production = Kohana::load(APPPATH.'config/env_production/database.php');
+		$this->view->development = Kohana::load(APPPATH.'config/env_development/database.php');
 		$this->view->original = Kohana::load(APPPATH.'config/database.php');
 
 		$this->response->body($this->view->render());
